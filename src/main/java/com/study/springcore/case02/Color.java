2308faed0d1d779a6101a739d64eda79a82683a5
stanley1208 <stanley.mei08@gmail.com>
2021-12-12 21:37:07 +0800

    java

---
+++ src/main/java/com/study/springcore/case02/Color.java
@@ -0,0 +1,32 @@
+package com.study.springcore.case02;
+
+public class Color {
+	private String code;
+
+	
+	
+	public Color() {
+		super();
+	}
+
+
+
+	public Color(String code) {
+		super();
+		this.code = code;
+	}
+
+
+
+	public String getCode() {
+		return code;
+	}
+
+
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	
+}
