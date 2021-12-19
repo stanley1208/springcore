47ba9c34e4fddaf32531f067e737f8b3ea6cfcd6
stanley1208 <stanley.mei08@gmail.com>
2021-12-19 17:28:26 +0800

    java

--- src/main/java/com/study/springcore/case07/Person.java
+++ src/main/java/com/study/springcore/case07/Person.java
@@ -6,90 +6,98 @@ import java.util.Map;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-
-// 在配置文件中有設定
-// <context:include-filter type="assignable" expression="com.study.springcore.case07.User"/>
-// 則可以不用加上@Component
 @Component
-public class User {
-	@Value(value = "John")
+@PropertySource("classpath:person.properties")
+public class Person {
+	
+	@Value("${person.name}")
 	private String name;
 	
-	@Value(value = "18")
+	@Value("${person.age}")
 	private Integer age;
 	
-	@Value("#{${nicknames:{'Happy','Enjoy'}}}") // Spring EL
+	@Value("${person.nicknames}")
 	private String[] nicknames;
 
-	@Value("#{${subjects:{'國文','英文'}}}")
+	@Value("${person.subjects}")
 	private Set<String>subjects;
 
-	@Value("#{${scores:{100,90,80}}}")
+	@Value("#{'${person.scores}'.split(',')}") // Spring EL
 	private List<String>scores;
 
-	@Value("#{${hobbies:{1:'Car',2:'Motor'}}}")
+	@Value("#{${person.hobbies}}") // Spring EL
 	private Map<Integer, String>hobbies;
+
 
 	public String getName() {
 		return name;
 	}
 
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 
 	public Integer getAge() {
 		return age;
 	}
 
+
 	public void setAge(Integer age) {
 		this.age = age;
 	}
+
 
 	public String[] getNicknames() {
 		return nicknames;
 	}
 
+
 	public void setNicknames(String[] nicknames) {
 		this.nicknames = nicknames;
 	}
+
 
 	public Set<String> getSubjects() {
 		return subjects;
 	}
 
+
 	public void setSubjects(Set<String> subjects) {
 		this.subjects = subjects;
 	}
+
 
 	public List<String> getScores() {
 		return scores;
 	}
 
+
 	public void setScores(List<String> scores) {
 		this.scores = scores;
 	}
+
 
 	public Map<Integer, String> getHobbies() {
 		return hobbies;
 	}
 
+
 	public void setHobbies(Map<Integer, String> hobbies) {
 		this.hobbies = hobbies;
 	}
 
+
 	@Override
 	public String toString() {
-		return "User [name=" + name + ", age=" + age + ", nicknames=" + Arrays.toString(nicknames) + ", subjects="
+		return "Person [name=" + name + ", age=" + age + ", nicknames=" + Arrays.toString(nicknames) + ", subjects="
 				+ subjects + ", scores=" + scores + ", hobbies=" + hobbies + "]";
 	}
-	
-	
-	
-	
-	
+
 	
 	
 }
