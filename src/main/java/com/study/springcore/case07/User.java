605e55bd265c77a986190780bdeff05e46d571d1
stanley1208 <stanley.mei08@gmail.com>
2021-12-19 16:56:08 +0800

    java

--- src/main/java/com/study/springcore/case07/User.java
+++ src/main/java/com/study/springcore/case07/User.java
@@ -1,5 +1,10 @@
 package com.study.springcore.case07;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +15,18 @@ public class User {
 	
 	@Value(value = "18")
 	private Integer age;
+	
+	@Value("#{${nicknames:{'Happy','Enjoy'}}}") // Spring EL
+	private String[] nicknames;
+
+	@Value("#{${subjects:{'國文','英文'}}}")
+	private Set<String>subjects;
+
+	@Value("#{${scores:{100,90,80}}}")
+	private List<String>scores;
+
+	@Value("#{${hobbies:{1:'Car',2:'Motor'}}}")
+	private Map<Integer, String>hobbies;
 
 	public String getName() {
 		return name;
@@ -27,10 +44,48 @@ public class User {
 		this.age = age;
 	}
 
+	public String[] getNicknames() {
+		return nicknames;
+	}
+
+	public void setNicknames(String[] nicknames) {
+		this.nicknames = nicknames;
+	}
+
+	public Set<String> getSubjects() {
+		return subjects;
+	}
+
+	public void setSubjects(Set<String> subjects) {
+		this.subjects = subjects;
+	}
+
+	public List<String> getScores() {
+		return scores;
+	}
+
+	public void setScores(List<String> scores) {
+		this.scores = scores;
+	}
+
+	public Map<Integer, String> getHobbies() {
+		return hobbies;
+	}
+
+	public void setHobbies(Map<Integer, String> hobbies) {
+		this.hobbies = hobbies;
+	}
+
 	@Override
 	public String toString() {
-		return "User [name=" + name + ", age=" + age + "]";
+		return "User [name=" + name + ", age=" + age + ", nicknames=" + Arrays.toString(nicknames) + ", subjects="
+				+ subjects + ", scores=" + scores + ", hobbies=" + hobbies + "]";
 	}
+	
+	
+	
+	
+	
 	
 	
 }
