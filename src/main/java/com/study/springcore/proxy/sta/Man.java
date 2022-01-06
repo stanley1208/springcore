27f5a40177258798eaa8db66df00a518c2936a3b
stanley1208 <stanley.mei08@gmail.com>
2022-01-06 22:06:55 +0800

    java

--- src/main/java/com/study/springcore/proxy/sta/Man.java
+++ src/main/java/com/study/springcore/proxy/sta/Man.java
@@ -4,11 +4,16 @@ public class Man implements Person {
 
 	@Override
 	public void work() {
-		// 公用方法
-		System.out.println("戴口罩");
+		// 公用邏輯(之前要做的準備)
+		//System.out.println("戴口罩");
+		// 公用邏輯(例外)
+		//System.out.println("若沒口罩，要去買口罩");
 		// 業務邏輯
 		System.out.println("上班工作");
-
+		// 業務邏-有例外發生
+		System.out.println(10/0);
+		// 公用邏輯(之後要做的準備)
+		//System.out.println("脫下口罩");
 	}
 
 }
--- src/main/java/com/study/springcore/proxy/sta/Woman.java
+++ src/main/java/com/study/springcore/proxy/sta/Woman.java
@@ -4,11 +4,14 @@ public class Woman implements Person {
 
 	@Override
 	public void work() {
-		// 公用方法
-		System.out.println("戴口罩");
+		// 公用邏輯
+		//System.out.println("戴口罩");
+		// 公用邏輯(例外)
+		//System.out.println("若沒口罩，要去買口罩");
 		// 業務邏輯
 		System.out.println("去市場買菜");
-
+		// 公用邏輯
+		//System.out.println("脫下口罩");
 	}
 
 }
