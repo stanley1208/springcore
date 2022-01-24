af1267564401cb7e72ec797110cb982c63c0aea4
stanley1208 <stanley.mei08@gmail.com>
2022-01-24 16:00:29 +0800

    java

--- src/main/java/com/study/springcore/tx/dao/BookDaoImpl.java
+++ src/main/java/com/study/springcore/tx/dao/BookDaoImpl.java
@@ -3,42 +3,59 @@ package com.study.springcore.tx.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-public class BookDaoImpl implements BookDao{
+public class BookDaoImpl implements BookDao {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public Integer getPrice(Integer bid) {
-		String sql="select price from book where bid=?";
-		return jdbcTemplate.queryForObject(sql, Integer.class,bid);
+		String sql = "select price from book where bid=?";
+		return jdbcTemplate.queryForObject(sql, Integer.class, bid);
 	}
 
 	@Override
 	public Integer getStockAmount(Integer bid) {
-		String sql="select amount from stock where bid=?";
-		return jdbcTemplate.queryForObject(sql, Integer.class,bid);
-	
+		String sql = "select amount from stock where bid=?";
+		return jdbcTemplate.queryForObject(sql, Integer.class, bid);
+
 	}
 
 	@Override
 	public Integer getWalletMoney(Integer wid) {
-		String sql="select money from wallet where wid=?";
-		return jdbcTemplate.queryForObject(sql, Integer.class,wid);
-	
+		String sql = "select money from wallet where wid=?";
+		return jdbcTemplate.queryForObject(sql, Integer.class, wid);
+
 	}
 
 	@Override
 	public Integer updateStock(Integer bid, Integer amount) {
+		// 確認該書的最新庫存量
+		Integer new_amount = getStockAmount(bid);
+		if (new_amount <= 0) {
+			throw new RuntimeException(String.format("此書號: %d 目前沒庫存，目前數量:%d", bid, new_amount));
+		} else if (new_amount < amount) {
+			throw new RuntimeException(String.format("此書號: %d 目前庫存不足，目前數量:%d，欲購買數量%d", bid, new_amount, amount));
+
+		}
 		// 修改庫存
-		String sql="update stock set amount=amount-? where bid=?";
+		String sql = "update stock set amount=amount-? where bid=?";
 		return null;
 	}
 
 	@Override
 	public Integer updateWallet(Integer wid, Integer money) {
+		// 先確認錢包裡面的餘額
+		Integer new_money = getWalletMoney(wid);
+		if (new_money <= 0) {
+			throw new RuntimeException(String.format("錢包號碼:%d目前沒餘額:%d", wid, new_money));
+
+		} else if (new_money < money) {
+			throw new RuntimeException(String.format("錢包號碼:%d餘額不足，目前餘額:%d，預扣款金額%d", wid, new_money, money));
+
+		}
 		// 修改餘額
-		String sql="select wallet set money =money=? where wid=?";
-		return jdbcTemplate.queryForObject(sql, Integer.class,wid);
-	
+		String sql = "select wallet set money =money=? where wid=?";
+		return jdbcTemplate.queryForObject(sql, Integer.class, wid);
+
 	}
 }
